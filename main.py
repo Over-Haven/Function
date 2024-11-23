bdd04857ba2955cf4085a2012c2d872de9b99503
Over-haven <tanushdas26@gmail.com>
2024-11-23 20:24:17 +0530

    Yoo!!

---
+++ main.py
@@ -0,0 +1,8 @@
+def well_wishes():
+    print("Hellow")
+    print("How are you")
+    print("You look bigger then last time")
+    print("Hope your day is well")
+    print("see you later")
+
+well_wishes()
